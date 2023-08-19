917dfcb4d5bc3908d66ffda5d9b34cc1fe5dad25
Omar <osm3000@Omars-MacBook-Air.local>
2023-08-19 17:30:07 +0200

    Adding an example for generating words

---
+++ examples/generate_words.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('..')
+
+from langlearncopilot.generators import generate_unique_words
+
+
+generate_unique_words(article="Bonjour, je m'appelle Jean. Je suis un étudiant à l'université de Paris.")
