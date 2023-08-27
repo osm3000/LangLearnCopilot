c1098d3ab95e73a1f1bde9970480e2d17f66e0df
Omar <osm3000@Omars-MacBook-Air.local>
2023-08-27 06:50:01 +0200

    Add an example for getting data from a webpage

---
+++ examples/data_from_webpage.py
@@ -0,0 +1,16 @@
+from langlearncopilot.parsers import get_text_from_webpage
+from langlearncopilot.generators import generate_unique_words
+
+
+def main():
+    # Get text from a webpage
+    text = get_text_from_webpage(
+        url="https://www.lemonde.fr/planete/article/2023/08/27/comment-les-parcs-nationaux-americains-tentent-de-faire-face-aux-effets-du-rechauffement-climatique_6186696_3244.html"
+    )
+    # Generate unique words from the text
+    words = generate_unique_words(article=text, language="french")
+    print(words)
+
+
+if __name__ == "__main__":
+    main()
