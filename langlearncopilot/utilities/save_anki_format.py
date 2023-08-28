e3956388e6a6314604cc41706427d58dbeab17f6
Omar <osm3000@Omars-MacBook-Air.local>
2023-08-28 23:25:26 +0200

    Adding the Save-to-Anki-format functionality

---
+++ langlearncopilot/utilities/save_anki_format.py
@@ -0,0 +1,29 @@
+import typing
+from ..datatypes import *
+
+
+def save_multi_phrase(
+    data: MULTIPLE_PHRASES_TRANSLATION,
+    file_path: TEXT,
+):
+    # Check if the data is a TEXT_TRANSLATION or MULTIPLE_PHRASES_TRANSLATION
+    final_data = []
+    for item in data:
+        final_data.append(f"{item['word']}; {item['phrase']}; {item['translation']}")
+
+    # Save the data to a csv file
+    with open(file_path, "w") as f:
+        f.write("\n".join(final_data))
+
+
+def save_unique_words(
+    data: TEXT_TRANSLATION,
+    file_path: TEXT,
+):
+    # Check if the data is a TEXT_TRANSLATION or MULTIPLE_PHRASES_TRANSLATION
+    final_data = []
+    for key, value in data.items():
+        final_data.append(f"{key}; {value}")
+
+    with open(file_path, "w") as f:
+        f.write("\n".join(final_data))
