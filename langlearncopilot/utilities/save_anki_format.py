7333916aa02ab17d26429e01caba2de452174827
Omar <osm3000@Omars-MacBook-Air.local>
2023-09-11 07:10:20 +0200

    The modifications in order to return the data and accept not having a file to save to

--- langlearncopilot/utilities/save_anki_format.py
+++ langlearncopilot/utilities/save_anki_format.py
@@ -1,9 +1,13 @@
-from ..datatypes import MULTIPLE_PHRASES_TRANSLATION, TEXT, TEXT_TRANSLATION
+from ..datatypes import (
+    MULTIPLE_PHRASES_TRANSLATION,
+    TEXT_TRANSLATION,
+    OPTIONAL_TEXT,
+)
 
 
 def save_multi_phrase(
     data: MULTIPLE_PHRASES_TRANSLATION,
-    file_path: TEXT,
+    file_path: OPTIONAL_TEXT = None,
 ):
     # Check if the data is a TEXT_TRANSLATION or MULTIPLE_PHRASES_TRANSLATION
     final_data = []
@@ -11,18 +15,25 @@ def save_multi_phrase(
         final_data.append(f"{item['word']}; {item['phrase']}; {item['translation']}")
 
     # Save the data to a csv file
-    with open(file_path, "w") as f:
-        f.write("\n".join(final_data))
+    if file_path is not None:
+        with open(file_path, "w") as f:
+            f.write("\n".join(final_data))
+
+    return final_data
 
 
 def save_unique_words(
     data: TEXT_TRANSLATION,
-    file_path: TEXT,
+    file_path: OPTIONAL_TEXT = None,
 ):
     # Check if the data is a TEXT_TRANSLATION or MULTIPLE_PHRASES_TRANSLATION
     final_data = []
     for key, value in data.items():
         final_data.append(f"{key}; {value}")
 
-    with open(file_path, "w") as f:
-        f.write("\n".join(final_data))
+    # Save the data to a csv file
+    if file_path is not None:
+        with open(file_path, "w") as f:
+            f.write("\n".join(final_data))
+
+    return final_data
